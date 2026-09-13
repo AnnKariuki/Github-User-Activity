cba77b5cdf06c4da1bac168715a3bf115d0af407
Ann Kariuki <ann.kariuki@duke.edu>
2026-09-13 16:35:58 -0700

    add caching

--- activity.py
+++ activity.py
@@ -1,13 +1,32 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 from dotenv_loader import API_KEY
 import argparse
 from urllib.request import urlopen, Request
 import json 
 from urllib.error import HTTPError, URLError
 from typing import Any
+import redis
 
-def get_github_activity(args: argparse.Namespace) -> None:
-    username = args.username
+r = redis.Redis(
+    host='localhost',
+    port=6379,
+    db=0 # The default Redis database index,
+)
+
+def get_user_data(username: str) -> bytes | None:
+    cache_key = f"user_activity:{username.lower()}"
+    # Always make sure there is connectivity with Redis before performing any redis operations
+    try:
+        # github usernames are case-insensitive so octat and Octat should be the same key. avoid data duplication
+        cached_data = r.get(cache_key)
+        if cached_data is not None:
+            print("Cache hit!")
+            return cached_data
+    except redis.ConnectionError as e:
+        print(f"❌ Connection error: Could not connect to Redis at {e}.")
+        print("Fall through to api call")
+
+    print("Cache miss")
     url = f"https://api.github.com/users/{username}/events"
     headers = {"Authorization": f"Bearer {API_KEY}"}
     request = Request(url, headers=headers)
@@ -18,20 +37,32 @@ def get_github_activity(args: argparse.Namespace) -> None:
         if error.code == 404:
             print(f"Github username {username} was not found")
         elif error.code == 401:
-            print(f"Unauthorized to carry out this action ")
+            print("Unauthorized to carry out this action")
         else:
             print(error.status, error.reason)
-        return
+        return None
     except URLError as error:
         print(error.reason)
-        return
+        return None
     except TimeoutError:
         print('Request Timed out')
-        return
+        return None
     else:
-        decoded_body = body.decode("utf-8")
+        try: 
+            r.set(cache_key, body, ex=300)
+        except redis.ConnectionError as e:
+            print(f"❌ Connection error: Could not connect to Redis at {e}.")
+        return body
+  
+def get_github_activity(args: argparse.Namespace) -> None:
+    username = args.username
+    user_data = get_user_data(username)
+    if user_data is not None:
+        decoded_body = user_data.decode("utf-8")
         events = json.loads(decoded_body)
         display_events(events)
+    else:
+        return None
 
 def format_event(event: dict[str,Any]) -> str:
     event_type = event["type"]
