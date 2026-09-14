beefa5bb76601d5dda1cc1474a4d214a201a006b
Ann Kariuki <ann.kariuki@duke.edu>
2026-09-13 17:11:56 -0700

    tests

--- test_activity.py
+++ test_activity.py
@@ -26,24 +26,71 @@ class TestActivity(unittest.TestCase):
         # response we expect
         cls.events_bytes = json.dumps(SAMPLE_EVENT).encode('utf-8')
 
+    # @patch("activity.urlopen")
+    # @patch("activity.display_events")
+    # def test_get_github_activity_success(self, mocked_display, mocked_urlopen):
+    #     # a mock is a blank slate and you can methods and attributes to it
+    #     # with urlopen returns a context manager
+    #     manager_mock = MagicMock()
+    #     #the enter function yields the response object
+    #     response_mock = manager_mock.__enter__.return_value = MagicMock()
+    #     response_mock.read.return_value = self.events_bytes
+    #     mocked_urlopen.return_value = manager_mock
+    #     args = argparse.Namespace(username="octocat")
+    #     activity.get_github_activity(args)
+    #     mocked_display.assert_called_once_with(SAMPLE_EVENT)
+
+
+    # @patch("activity.urlopen")
+    # @patch("activity.print")
+    # def test_get_github_activity_404(self, mock_print, mocked_urlopen):
+    #     # no manager or response mock here urelopen fails
+    #     mocked_urlopen.side_effect = HTTPError(
+    #         url="https://api.github.com/users/notarealusername/events",
+    #         code=404,
+    #         msg="Not Found",
+    #         hdrs=None,
+    #         fp=None,
+    #     )
+    #     args = argparse.Namespace(username="notarealusername")
+    #     activity.get_github_activity(args)
+    #     mock_print.assert_called_once_with(
+    #         "Github username notarealusername was not found"
+    #     )
+
+    # NEW TESTS SINCE WE ADDED CACHING
+    @patch("activity.r")
     @patch("activity.urlopen")
-    @patch("activity.display_events")
-    def test_get_github_activity_success(self, mocked_display, mocked_urlopen):
-        # a mock is a blank slate and you can methods and attributes to it
-        # with urlopen returns a context manager
+    def test_get_user_data_cache_miss(self, mocked_urlopen, mocked_redis):
+        mocked_redis.get.return_value = None
+        # we need to mock url open here. we do not want the test to make the direct call to github.
+        # github can rate-limit us, the user activity can change, network may fail making our test nondeterministic and flaky
         manager_mock = MagicMock()
-        #the enter function yields the response object
-        response_mock = manager_mock.__enter__.return_value = MagicMock()
+        response_mock = MagicMock()
+        manager_mock.__enter__.return_value = response_mock
         response_mock.read.return_value = self.events_bytes
         mocked_urlopen.return_value = manager_mock
-        args = argparse.Namespace(username="octocat")
-        activity.get_github_activity(args)
-        mocked_display.assert_called_once_with(SAMPLE_EVENT)
 
+        result = activity.get_user_data("octat")
+        self.assertEqual(result, self.events_bytes)
+
+    @patch("activity.r")
+    @patch("activity.urlopen")
+    def test_get_user_data_cache_hit(self, mocked_urlopen, mocked_redis):
+        mocked_redis.get.return_value = self.events_bytes
+
+        result = activity.get_user_data("Octocat")
+
+        self.assertEqual(result, self.events_bytes)
+        mocked_redis.get.assert_called_once_with("user_activity:octocat")
+        mocked_urlopen.assert_not_called()
+
+    @patch("activity.r")
     @patch("activity.urlopen")
     @patch("activity.print")
-    def test_get_github_activity_404(self, mock_print, mocked_urlopen):
-        # no manager or response mock here urelopen fails
+    def test_get_user_data_404(self, mocked_print, mocked_urlopen, mocked_redis):
+        mocked_redis.get.return_value = None
+
         mocked_urlopen.side_effect = HTTPError(
             url="https://api.github.com/users/notarealusername/events",
             code=404,
@@ -51,11 +98,21 @@ class TestActivity(unittest.TestCase):
             hdrs=None,
             fp=None,
         )
-        args = argparse.Namespace(username="notarealusername")
-        activity.get_github_activity(args)
-        mock_print.assert_called_once_with(
+
+        result = activity.get_user_data("notarealusername")
+
+        self.assertIsNone(result)
+        mocked_print.assert_any_call(
             "Github username notarealusername was not found"
         )
+
+    @patch("activity.get_user_data")
+    @patch("activity.display_events")
+    def test_get_github_activity_success(self, mocked_display, mocked_user_get_data):
+        mocked_user_get_data.return_value = self.events_bytes
+        args = argparse.Namespace(username="octocat")
+        activity.get_github_activity(args)
+        mocked_display.assert_called_once_with(SAMPLE_EVENT)
 
     def test_format_event_success(self):
         self.assertEqual(activity.format_event(SAMPLE_EVENT[0]),"octocat starred octocat/Hello-World" )
